32a1185f9c25dd3cf9a142b023d0d83586f7e1e9
TOVAR790 <87555146+TOVAR790@users.noreply.github.com>
2021-10-10 23:39:25 -0500

    Add files via upload

---
+++ js/script1.js
@@ -0,0 +1,13 @@
+function Goodbye(person) {
+  var JoseGoodbye = {};
+  //yakovGreeter.name = "Yaakov";
+  /*var speakWord = "Hello ";*/
+  //var yakov = "Hello"
+  //yakovGreeter.sayHello = function (Yaakov) {
+    console.log('Goodbye '  +  person);
+  }
+
+  Goodbye('Jose');
+  Goodbye('Jorge');
+
+
