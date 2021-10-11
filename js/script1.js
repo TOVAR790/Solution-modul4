72c33976007263fcf831d0e3c3318f4788d53f04
TOVAR790 <87555146+TOVAR790@users.noreply.github.com>
2021-10-11 09:57:13 -0500

    Update script1.js

--- js/script1.js
+++ js/script1.js
@@ -1,13 +1,28 @@
-function Goodbye(person) {
-  var JoseGoodbye = {};
-  //yakovGreeter.name = "Yaakov";
-  /*var speakWord = "Hello ";*/
-  //var yakov = "Hello"
-  //yakovGreeter.sayHello = function (Yaakov) {
-    console.log('Goodbye '  +  person);
-  }
 
-  Goodbye('Jose');
-  Goodbye('Jorge');
+ //var AndresGreeter = {};
+  //yakovGreeter.name = "Yaakov";
+
+var names=new Array();
+names[0]="Andres";
+names[1]="John";
+names[2]="Jen";
+names[3]="jason";
+names[4]="paul";
+names[5]="frank";
+names[6]="larry";
+names[7]="paula";
+names[8]="laura";
+names[9]="jim";
+
+
+for (var i = 0; i < names.length; i++) {
+	if(names[i].charAt(0)==='J'|| names[i].charAt(0)==='j'){
+        console.log("Goodbye "+ names[i])
+	}
+	else{
+		console.log("Hello "+ names[i])
+	}
+}
+
 
 
